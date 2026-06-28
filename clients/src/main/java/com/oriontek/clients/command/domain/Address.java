160fb2af0d65662a5e56af1ab73a4a5746fb4c48
Bryan Bocio <iqtek9@cibao.com.do>
2026-06-28 12:42:28 -0400

    agregando dominio de address con su config completa

--- clients/src/main/java/com/oriontek/clients/command/domain/Address.java
+++ clients/src/main/java/com/oriontek/clients/command/domain/Address.java
@@ -1,5 +1,35 @@
 package com.oriontek.clients.command.domain;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "address")
 public class Address {
-    
+    @Id
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @Column(nullable = false, length = 255)
+    private String street;
+
+    @Column(length = 120)
+    private String city;
+
+    @Column(length = 120)
+    private String country;
+
+    protected Address() {
+     
+    }
 }
