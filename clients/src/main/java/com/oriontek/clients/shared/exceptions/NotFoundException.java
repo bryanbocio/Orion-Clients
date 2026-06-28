1ce715e7cf2f0f2e71e961328e43ffbd7e0f8764
Bryan Bocio <iqtek9@cibao.com.do>
2026-06-28 13:41:36 -0400

    argegando clase para entidades no encontradas

---
+++ clients/src/main/java/com/oriontek/clients/shared/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.oriontek.clients.shared.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+    
+}
