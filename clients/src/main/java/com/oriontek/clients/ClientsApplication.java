7f402acd6c7281fe11e4cd6758dff3c2ed572a7b
Bryan Bocio <iqtek9@cibao.com.do>
2026-06-28 15:47:19 -0400

    agregando anotacion para que se pueda ejecutar el worker

--- clients/src/main/java/com/oriontek/clients/ClientsApplication.java
+++ clients/src/main/java/com/oriontek/clients/ClientsApplication.java
@@ -2,8 +2,10 @@ package com.oriontek.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ClientsApplication {
 
 	public static void main(String[] args) {
