979fafd3d6e8a30fce3497670059a5a35b9527a4
Bryan Bocio <iqtek9@cibao.com.do>
2026-06-28 13:38:22 -0400

    argegando metodo para crear clientes

--- clients/src/main/java/com/oriontek/clients/command/service/ClientCommandService.java
+++ clients/src/main/java/com/oriontek/clients/command/service/ClientCommandService.java
@@ -1,5 +1,35 @@
 package com.oriontek.clients.command.service;
 
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oriontek.clients.command.domain.Client;
+import com.oriontek.clients.command.repository.IAddressRepository;
+import com.oriontek.clients.command.repository.IClientRepository;
+import com.oriontek.clients.outbox.OutboxRepository;
+
+import jakarta.transaction.Transactional;
+
 public class ClientCommandService {
     
+     private final IClientRepository clientRepository;
+    private final IAddressRepository addressRepository;
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
+
+
+    public ClientCommandService(IClientRepository clientRepository, IAddressRepository addressRepository, OutboxRepository outboxRepository, ObjectMapper objectMapper) {
+        this.clientRepository = clientRepository;
+        this.addressRepository = addressRepository;
+        this.outboxRepository = outboxRepository;
+        this.objectMapper = objectMapper;
+    }
+
+
+    @Transactional
+    public Client createClient(String name) {
+        Client client = new Client(UUID.randomUUID(), name);
+        clientRepository.save(client);
+        return client;
+    }
 }
