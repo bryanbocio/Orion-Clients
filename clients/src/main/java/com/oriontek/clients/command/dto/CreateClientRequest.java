f04cca627fb17d88200ef2275d4d603df30d9c94
Bryan Bocio <iqtek9@cibao.com.do>
2026-06-28 18:50:22 -0400

    fix typo

--- clients/src/main/java/com/oriontek/clients/command/dto/CreateClientRequest.java
+++ clients/src/main/java/com/oriontek/clients/command/dto/CreateClientRequest.java
@@ -1,10 +1,8 @@
 package com.oriontek.clients.command.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateClientRequest(
-        @NotBlank(message = "The name is required")
         @Size(max = 150, message = "lenght can not be more than 150")
         String name
 ) {
