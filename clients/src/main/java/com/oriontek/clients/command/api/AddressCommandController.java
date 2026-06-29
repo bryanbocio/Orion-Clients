42e368417975f1de7d2bc25d97298d74dedb1f74
Bryan Bocio <iqtek9@cibao.com.do>
2026-06-28 21:56:39 -0400

    agregando restcontroller anotation

--- clients/src/main/java/com/oriontek/clients/command/api/AddressCommandController.java
+++ clients/src/main/java/com/oriontek/clients/command/api/AddressCommandController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.oriontek.clients.command.dto.AddAddressRequest;
 import com.oriontek.clients.command.dto.AddressResponse;
@@ -17,6 +19,8 @@ import com.oriontek.clients.command.service.ClientCommandService;
 
 import jakarta.validation.Valid;
 
+@RestController
+@RequestMapping("/api/v1/clients/{clientId}/addresses")
 public class AddressCommandController {
       private final ClientCommandService service;
 
